8cae27902f7a7f85daab70ac42bd3d2e163aed95
Daniel <danielarn1799@gmail.com>
2019-03-13 11:15:54 -0400

    Initial Commit

---
+++ src/Rotor.java
@@ -0,0 +1,23 @@
+public class Rotor {
+    int mPosition = 0;
+    int mRotorNumber;
+    int mRotorPosition;
+
+    public Rotor(int rotorPosition, int rotorNumber) {
+        mRotorPosition = rotorPosition;
+        mRotorNumber = rotorNumber;
+
+        switch (rotorNumber) {
+            case 0:
+
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+        }
+    }
+}
