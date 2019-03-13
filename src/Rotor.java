1fea8f98bc8d7a63c6e14e46f8157bc5c2b8700a
Daniel <danielarn1799@gmail.com>
2019-03-13 11:29:05 -0400

    Added the five standard rotors and cyphers

--- src/Rotor.java
+++ src/Rotor.java
@@ -2,6 +2,9 @@ public class Rotor {
     int mPosition = 0;
     int mRotorNumber;
     int mRotorPosition;
+    String cypher;
+    String notch;
+    String turnover;
 
     public Rotor(int rotorPosition, int rotorNumber) {
         mRotorPosition = rotorPosition;
@@ -9,14 +12,29 @@ public class Rotor {
 
         switch (rotorNumber) {
             case 0:
+                cypher = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+                notch = "Y";
+                turnover = "Q";
 
             case 1:
+                cypher = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+                notch = "M";
+                turnover = "E";
 
             case 2:
+                cypher = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+                notch = "D";
+                turnover = "V";
 
             case 3:
+                cypher = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
+                notch = "R";
+                turnover = "J";
 
             case 4:
+                cypher = "VZBRGITYUPSDNHLXAWMJQOFECK";
+                notch = "H";
+                turnover = "Z";
 
         }
     }
